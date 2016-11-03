104f81bb06272e106ca8e7b383171ba3632e191f
Daniel Quinn <code@danielquinn.org>
2016-11-03 17:11:11 +0000

    Added Parcelforce

--- kiki.py
+++ kiki.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
-import requests
+import re
 import sys
+
+import requests
 
 from bs4 import BeautifulSoup
 
 
+class UnknownShipperException(Exception):
+    pass
+
+
 class Shipper(object):
 
-    URL = ""
+    REGEX = None
+    URL = None
     USER_AGENT = "Kiki (https://github.com/danielquinn/kiki)"
+    HEADERS = {
+        "User-Agent": USER_AGENT
+    }
 
     def __init__(self, tracking_number):
         self.tracking_number = tracking_number
@@ -26,19 +36,31 @@ class Shipper(object):
 
     @classmethod
     def guess(cls, tracking_number):
-        return Eurodis(tracking_number)
+        """
+        Attempt to guess the right shipper based on the tracking number.
+        :return an instance of a Shipper subclass
+        """
+
+        if Parcelforce.REGEX.match(tracking_number):
+            return Parcelforce(tracking_number)
+
+        if Eurodis.REGEX.match(tracking_number):
+            return Eurodis(tracking_number)
+
+        raise UnknownShipperException(
+            "From the looks of that shipping number, it's hard to discern "
+            "what the shipper is."
+        )
 
 
 class Eurodis(Shipper):
 
+    REGEX = re.compile(r"^[0-9]{24}$")
     URL = "http://eurodis.com/track-trace?{id}"
 
     def fetch(self):
         url = self.URL.format(id=self.tracking_number)
-        headers = {
-            "User-Agent": self.USER_AGENT
-        }
-        return requests.get(url, headers=headers).text
+        return requests.get(url, headers=self.HEADERS).text
 
     def parse(self, page):
 
@@ -49,16 +71,43 @@ class Eurodis(Shipper):
             .find_all("td")
 
         return {
-            "location": "{} {}: {}, {} ({})".format(
-                location_soup[0].text,
-                location_soup[1].text,
-                location_soup[2].text,
-                location_soup[3].text,
-                location_soup[4].text,
+            "location": "{date} {time}: {city}, {country} ({event})".format(
+                date=location_soup[0].text,
+                time=location_soup[1].text,
+                city=location_soup[3].text,
+                country=location_soup[2].text,
+                event=location_soup[4].text,
             )
         }
 
 
+class Parcelforce(Shipper):
+
+    REGEX = re.compile("^[A-Z]{2}[0-9]{7}$")
+    URL = "http://www.parcelforce.com/track-trace?trackNumber={id}"
+
+    def fetch(self):
+        url = self.URL.format(id=self.tracking_number)
+        return requests.get(url, headers=self.HEADERS).text
+
+    def parse(self, page):
+
+        soup = BeautifulSoup(page, "html.parser")
+        location_soup = soup\
+            .find_all(class_="tracking-history")[0]\
+            .find_all("tbody")[0]\
+            .find_all("tr")[0]\
+            .find_all("td")
+
+        return {
+            "location": "{date} {time}: {city} ({event})".format(
+                date=location_soup[0].text,
+                time=location_soup[1].text,
+                city=location_soup[2].text,
+                event=location_soup[3].text,
+            )
+        }
+
 if __name__ == "__main__":
     location = Shipper.guess(sys.argv[1]).where_is()
-    print("It looks like your package is here:\n\n  {}".format(location))
+    print("\n  It looks like your package is here:\n\n  {}\n".format(location))
